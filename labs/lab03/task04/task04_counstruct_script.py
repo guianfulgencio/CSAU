aa653f2a4aa43ccca221f028311fafc214196e9a
Guian <guian_fulgencio@yahoo.com>
2023-04-03 15:07:03 +0800

    updates

--- labs/lab03/task04/task04_counstruct_script.py
+++ labs/lab03/task04/task04_counstruct_script.py
@@ -1,20 +1,20 @@
-# from netmiko import ConnectHandler
+from netmiko import ConnectHandler
 
 inventory = {
     "csr1kv1": {
-        "ip_addr": "10.254.0.1",
+        "ip_addr": "172.20.10.13",
         "username": "cisco",
         "password": "cisco",
         "device_type": "cisco_xe",
     },
     "csr1kv2": {
-        "ip_addr": "10.254.0.2",
+        "ip_addr": "172.20.10.7",
         "username": "cisco",
         "password": "cisco",
         "device_type": "cisco_xe",
     },
     "csr1kv3": {
-        "ip_addr": "10.254.0.3",
+        "ip_addr": "172.20.10.12",
         "username": "cisco",
         "password": "cisco",
         "device_type": "cisco_xe",
@@ -47,36 +47,36 @@ ip_info = {
 }
 
 # Templates for interface and IP configuration
-interface_template = "interface {}"
-interface_ip_template = "ip address {} {}"
+#show_run_template = "show run {} "
+#interface_template = f"interface {interface}"
+#interface_ip_template = f"ip address {details["ip"]} {details["mask"]}"
 
 # Iterating over ip_info dictionary an
 for hostname, ip_data in ip_info.items():
-    username = inventory[hostname][""]  # The hostname is used to obtain the value of username from inventory variable
-    password = inventory[hostname][""]  # The hostname is used to obtain the value of password from inventory variable
-    device_type = inventory[hostname][""] # The hostname is used to obtain the value of device_type from inventory variable
+    username = inventory[hostname]["username"]  # The hostname is used to obtain the value of username from inventory variable
+    password = inventory[hostname]["password"]  # The hostname is used to obtain the value of password from inventory variable
+    device_type = inventory[hostname]["device_type"] # The hostname is used to obtain the value of device_type from inventory variable
 
-    #device = ConnectHandler(
-    #    host=hostname,
-    #    username=username,
-    #    password=password,
-    #    device_type=device_type,
-    #)  # Initiating a connection to the device
+    print(f"Establishing connection with {hostname}")
+    device = ConnectHandler(
+        host=inventory[hostname]["ip_addr"],
+        username=username,
+        password=password,
+        device_type=device_type,
+    )  # Initiating a connection to the device
     for interface, details in ip_data.items():
         commands = []
-        interface_command = interface_template.format(
-            interface
-        )
+        interface_command = f"interface {interface}"
         commands.append(interface_command)
-        interface_ip_command = interface_ip_template.format(
-            details["ip"], details["mask"]
-        )
+        interface_ip_command = f"ip address {details['ip']} {details['mask']}"
         commands.append(interface_ip_command)
-    # print(commands) # print the commands and make sure the format is correct
 
-    # device.send_config_set(commands)
-
-    # show_command = show_run_template.format(commands[0])
-    # show_run_output = device.send_command(show_command)
-    # print(show_run_output)
-    # print("Disconnecting from the {} ".format(hostname))
+    print(commands) # print the commands and make sure the format is correct
+    print("Applying the configuration")
+    device.send_config_set(commands)
+    print("Validating if the configuration was successfully applied")
+    show_command = f"show run {commands[0]}"
+    show_run_output = device.send_command(show_command)
+    print(show_run_output)
+    print(f"Disconnecting from the {hostname}")
+    device.disconnect()
